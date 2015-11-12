58be646aea7931c1118fc9e5b140705cfb3ca8ed
bspavel <bspavel@i.ua>
2015-11-12 16:22:12 +0200

    commit 2015.11.12 16:22

--- my/config/auth.php
+++ my/config/auth.php
@@ -13,31 +13,37 @@ return [
         'facebook' => [
             // register your app here: https://developers.facebook.com/apps/
             'class' => 'nodge\eauth\services\FacebookOAuth2Service',
-            'clientId' => '...',
-            'clientSecret' => '...',
+            'clientId' => '1657698847844895',
+            'clientSecret' => '80e572561ddb517da2bff30e53f9f017',
         ],
 
         'vkontakte' => [
             // register your app here: https://vk.com/editapp?act=create&site=1
             'class' => 'nodge\eauth\services\VKontakteOAuth2Service',
-            'clientId' => '5129822',
-            'clientSecret' => 'IoCs27vO0tfd7USoTbK4',
+
+//            'clientId' => '5129822',
+//            'clientSecret' => 'IoCs27vO0tfd7USoTbK4',
+
             //'clientId' => '5143320',
             //'clientSecret' => 'O7vsoqQCG5q8fy6QHjUJ',
+            /***********************************************/
+            'clientId' => '5130699',
+            'clientSecret' => '4B25v7qHSl0NODAekOuh',
+            /***********************************************/
 
         ],
         'google' => [
             // register your app here: https://code.google.com/apis/console/
             'class' => 'nodge\eauth\services\GoogleOAuth2Service',
-            'clientId' => '...',
-            'clientSecret' => '...',
+            'clientId' => 'glowing-run-111709',
+            'clientSecret' => 'AIzaSyCCzNaYZQEM6j_WYHlfzr_KEpfFA6C1y24',
             'title' => 'Google',
         ],
         'linkedin_oauth2' => [
             // register your app here: https://www.linkedin.com/secure/developer
             'class' => 'nodge\eauth\services\LinkedinOAuth2Service',
-            'clientId' => '...',
-            'clientSecret' => '...',
+            'clientId' => '7724a4sbrutauu',
+            'clientSecret' => 'nxRkYB8vK6MxxvcL',
             'title' => 'LinkedIn (OAuth2)',
         ],
     ],
