ea4010fb8d95e6661c1de338e97fb4083f86c3ae
bspavel <bspavel@i.ua>
2015-11-23 23:27:56 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	my/.idea/encodings.xml
    	my/web/index.php

--- my/web/index.php
+++ my/web/index.php
@@ -1,13 +1,8 @@
 <?php
 
 // comment out the following two lines when deployed to production
-<<<<<<< HEAD
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-=======
  defined('YII_DEBUG') or define('YII_DEBUG', true);
  defined('YII_ENV') or define('YII_ENV', 'dev');
->>>>>>> origin/master
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
