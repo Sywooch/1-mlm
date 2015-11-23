d81e84ddbaf0103943384cd0862fa5341d5be672
bspavel <bspavel@i.ua>
2015-11-23 11:26:16 +0200

    commit 2015.11.23 11.26

--- my/views/site/_team_edit.php
+++ my/views/site/_team_edit.php
@@ -12,7 +12,7 @@
 <!--echo $dt["fn"], ' ', $dt["ln"];-->
 <div class="row">
     <div class="col-md-4">
-        <img style="border-radius: 15px;" src="mp.php/<?=$dt['userpic']?>" height="150">
+        <img style="border-radius: 21px !important;" src="mp.php/<?=$dt['userpic']?>" height="150">
     </div>
     <div class="col-md-8" style="text-align: left; margin-left: -18px;">
         Имя: <?=$dt["fn"]?><br>
