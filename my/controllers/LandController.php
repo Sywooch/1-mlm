697c2246d7c71ec672b0968f53dceac84d58faca
bspavel <bspavel@i.ua>
2015-12-10 16:10:35 +0200

    commit 2015.12.10 16:10

--- my/controllers/LandController.php
+++ my/controllers/LandController.php
@@ -10,12 +10,10 @@ class LandController extends Controller
     {
         $this->layout = "empty";
         $landid = (int)\Yii::$app->request->get("landid");
-        $uid = (int)\Yii::$app->request->get("uid");
-        $query=new \yii\db\Query();
-        $data=$query->from([Lp::tableName()])
+        $uid = (string)\Yii::$app->request->get("uid");
+        $data=(new \yii\db\Query())->from([Lp::tableName()])
             ->where(['id' => $landid])
             ->one();
-
         $lp=Lp::findOne(['id' => $landid]);
         $lp->clicks=++$lp->clicks;
         $lp->update(false);
@@ -28,7 +26,6 @@ class LandController extends Controller
         {
             $usr = Users::find()->where(['refdt' => $uid])->one();
         }
-
         return $this->render('land_'.$data["landtype"], [
             'data'=>$data,
             'user'=>$usr
