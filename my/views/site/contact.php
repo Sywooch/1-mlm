be0191c89f2e36245a30b4709d02d455a2bd1daf
Vitaliy <1977kvl@gmail.com>
2015-12-12 14:14:03 +0200

    commit  2015.12.12 14:14

--- my/views/site/contact.php
+++ my/views/site/contact.php
@@ -3,11 +3,9 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
 
-$this->title = 'Contact';
+$this->title = 'Контакт - 1 mlm ресурс';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-
-<br><br><br><br><br>
 
 <style>
     #section2 {
@@ -99,73 +97,53 @@ $this->params['breadcrumbs'][] = $this->title;
         height: 25px;
     }
 </style>
-
-
 <!--<link href="https://hostpro.ua/wp-content/themes/hostpro/assets/css/style.css" rel="stylesheet">-->
+<div class="row">
+    <div class="col-md-12">
+        <div class="portlet light bordered">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="icon-call-end font-blue-sharp"></i>
+                    <span class="caption-subject font-blue-sharp"><?= $this->title; ?></span>
+                </div>
+                <div align="right">
+                    <a class="btn btn-circle btn-icon-only btn-default" data-toggle="modal"
+                       data-target="#w1help" href="#w1help">
+                        <i class="icon-support"></i></a>
+                    <a title="" data-original-title=""
+                       class="btn btn-circle btn-icon-only btn-default fullscreen" href="javascript:;"> </a>
 
+                    <div style="display: none;" id="w1help" class="fade modal" role="dialog" tabindex="-1">
+                        <div class="modal-dialog ">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"
+                                            aria-hidden="true">×</button>
+                                    <h4 style="margin-top: 0px;"><div align="center">Помощь</div></h4>
+                                </div>
+                                <div class="modal-body">
+                                    <iframe width="560" height="315"
+                                            src="https://www.youtube-nocookie.com/embed/<?php
+                                            echo "iBfk37Fa3H0";
+                                            ?>?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                    <!------------------------------------------------------------>
 <section id="section2" data-scen-id="0" class="builder-section no-padding bg-img ng-touched ng-dirty-parse" ng-mouseup="deTextCustomize()">
     <div class="wrap">
         <div class="white-block">
             <h3>Есть вопросы?</h3>
             <p>Обращайтесь в техподдержку</p>
             <p>с 10:00 до 20:00</p>
-            <span class="phone">(067) 233-67-55</span>
+            <!--<span class="phone">(067) 233-67-55</span>-->
             <span class="phone">support@1-mlm.com</span>
             <div><a class="skype" href="skype:support.mlm?chat" style="" data-edit-now="false"><i class="fa fa-skype"></i> support.mlm</a></div>
             <br></div>
     </div>
 </section>
 
-
-
-
-
-<!--<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
-
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
-
-    <?php else: ?>
-
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
-
-        <div class="row">
-            <div class="col-lg-5">
-
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-                    <?= $form->field($model, 'name') ?>
-                    <?= $form->field($model, 'email') ?>
-                    <?= $form->field($model, 'subject') ?>
-                    <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
-                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div>
-                        <div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
-                    <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
                     </div>
-                <?php ActiveForm::end(); ?>
-
-            </div>
-        </div>
-
-    <?php endif; ?>
-</div>-->
