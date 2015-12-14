09d6b6860b0d5a1804ea41ff93ce8b064010ed92
bspavel <bspavel@i.ua>
2015-12-14 13:36:44 +0200

    commit 2015.12.14 13:36

---
+++ my/views/pay/m.php
@@ -0,0 +1,220 @@
+<div class="pricing-content-1">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="price-column-container border-active">
+                <div class="price-table-head bg-blue">
+                    <h2 class="no-margin">Новичек</h2>
+                </div>
+                <div class="arrow-down border-top-blue"></div>
+                <div class="price-table-pricing">
+                    <h3>
+                        <small><strike style="color: #959695; font-size: 23px;">2$</strike></small> 0<span class="price-sign">&nbsp;$</span></h3>
+                    <p>в месяц</p>
+                </div>
+                <div class="price-table-content">
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-rocket"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Страница - 1 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-briefcase"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Компания - 1 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-magnet"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Мастер Класс - 0 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-close"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Партнерка - нет </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-close"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Поддержка - нет </div>
+                    </div>
+                </div>
+                <div class="arrow-down arrow-grey"></div>
+                <div class="price-table-footer">
+                    <button type="button" class="btn grey-salsa btn-outline sbold uppercase price-button">Выбрать</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="price-column-container border-active">
+                <div class="price-table-head bg-red">
+                    <h2 class="no-margin">Мастер</h2>
+                </div>
+                <div class="arrow-down border-top-red"></div>
+                <div class="price-table-pricing">
+                    <h3>
+                        <small><strike style="color: #959695; font-size: 23px;">10$</strike></small> 2<span class="price-sign">&nbsp;$</span></h3>
+                    <p>в месяц</p>
+                </div>
+                <div class="price-table-content">
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-rocket"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Страниц - 3 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-briefcase"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Компания - 3 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-magnet"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Мастер Класс - да </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-wallet"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Партнерка - да </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-envelope-letter"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Поддержка - e-mail </div>
+                    </div>
+                </div>
+                <div class="arrow-down arrow-grey"></div>
+                <div class="price-table-footer">
+
+                    <form id="" action="https://www.liqpay.com/api/checkout" method="post" accept-charset="utf-8">
+                        <?= $btn2; ?>
+                        <button type="submit" class="btn grey-salsa btn-outline price-button sbold uppercase">
+                            Выбрать</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="price-column-container border-active">
+                <div class="price-table-head bg-green">
+                    <h2 class="no-margin">Лидер</h2>
+                </div>
+                <div class="arrow-down border-top-green"></div>
+                <div class="price-table-pricing">
+                    <h3>
+                        <small><strike style="color: #959695; font-size: 23px;">25$</strike></small> 10<span class="price-sign">$</span></h3>
+                    <p>в месяц</p>
+                    <div class="price-ribbon">Лучший</div>
+                </div>
+                <div class="price-table-content">
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-rocket"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Страниц - 10 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-briefcase"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Компания - 10 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-magnet"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Мастер Класс - да </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-wallet"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Партнерка - да </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-earphones-alt"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Поддержка - skype </div>
+                    </div>
+                </div>
+                <div class="arrow-down arrow-grey"></div>
+                <div class="price-table-footer">
+
+                    <form id="" action="https://www.liqpay.com/api/checkout" method="post" accept-charset="utf-8">
+                        <?= $btn10; ?>
+                        <button type="submit" class="btn green price-button sbold uppercase">
+                            Выбрать</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="price-column-container border-active">
+                <div class="price-table-head bg-purple">
+                    <h2 class="no-margin">Профи</h2>
+                </div>
+                <div class="arrow-down border-top-purple"></div>
+                <div class="price-table-pricing">
+                    <h3>
+                        <small><strike style="color: #959695; font-size: 23px;">50$</strike></small>  25<span class="price-sign">&nbsp;$</span></h3>
+                    <p>в месяц</p>
+                </div>
+                <div class="price-table-content">
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-rocket"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Страниц - 25 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-briefcase"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Компания - 25 </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-magnet"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Мастер Класс - да </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-wallet"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Партнерка - да </div>
+                    </div>
+                    <div class="row mobile-padding">
+                        <div class="col-xs-3 text-right mobile-padding">
+                            <i class="icon-earphones-alt"></i>
+                        </div>
+                        <div class="col-xs-9 text-left mobile-padding"> Поддержка - skype </div>
+                    </div>
+                </div>
+                <div class="arrow-down arrow-grey"></div>
+                <div class="price-table-footer">
+                    <form id="" action="https://www.liqpay.com/api/checkout" method="post" accept-charset="utf-8">
+                        <?= $btn25; ?>
+                        <button type="submit"
+                                class="btn grey-salsa btn-outline price-button sbold uppercase">
+                            Выбрать</button>
+                    </form>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
