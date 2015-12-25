d6569c583ef32a149cc74478bd1b7922332b4957
Vitaliy <1977kvl@gmail.com>
2015-12-25 18:20:02 +0200

    commit  2015.12.25  18:19

---
+++ my/controllers/OplataController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class OplataController extends \yii\web\Controller
+{
+    public function actionOplata()
+    {
+        return $this->render('oplata');
+    }
+
+}
---
+++ my/views/oplata/oplata.php
@@ -0,0 +1,11 @@
+<?php
+$this->registerJsFile('/mertonic/global/scripts/app_acc.js');
+
+$this->registerJsFile('/mertonic/pages/scripts/dashboard.js', ['depends' => 'yii\web\JqueryAsset']);
+$this->registerJsFile('/mertonic/layouts/layout4/scripts/layout.js', ['depends' => 'yii\web\JqueryAsset']);
+$this->registerJsFile('/mertonic/layouts/layout4/scripts/demo.js', ['depends' => 'yii\web\JqueryAsset']);
+$this->registerJsFile('/mertonic/layouts/global/scripts/quick-sidebar.js', ['depends' => 'yii\web\JqueryAsset']);
+
+$this->title = 'Оплата';
+?>
+Страница для просмотра оплат в системе )
