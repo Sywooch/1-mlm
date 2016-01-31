9e629e3b5c561f74d282822e01df6ae69bd2579a
Vitaliy <1977kvl@gmail.com>
2016-01-31 14:35:23 +0200

    Commit 31.01.2016  14:35

---
+++ my/views/land/land_5.php
@@ -0,0 +1,398 @@
+<!doctype html>
+<html>
+<head>
+<script>
+
+</script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<title>Hostrocket</title>
+<link rel="stylesheet" type="text/css" href="../s/land_5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../s/land_5/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="../s/land_5/css/style.css">
+
+<link type="image/x-icon" rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABKUlEQVQ4jb3SzyvlURjH8VdYiKRrrh/ZzP9AtsJfwHYm1CT8ARQ7ljasLPwJmt1sLGz8aGzUdJMuM/KjFPmxkMRkgcV51O3mq1vKU6fO0znvz/N8nnP4xBjCbzzgHw4qBfNYwT2WMYIOnFQCt2IPRXTjF+bxHY9ZUA168AOnAefQjP0Q+YntcjCHCcnfc6xdNJTcaUcvnvCtvOp6tLWEaxTQiK4Qfy2yh9Xy6lUYxR2OsIk6jEUnnWGhiL9oyfI/IE17AsMBj0flgjT5tiy4DXO4KpnBIGrxp0TszfgSrZ1hQ/osz5iM/CLyviyBKdxKQ4M1XAZ0jtkQyfR+iOnY56OTOfSH8H8sZMGkd13EDG6kD/M1zkawhab3BI4l3zuS7/qy8+r34A/FC5cqTDh6ZYEVAAAAAElFTkSuQmCC">
+
+</head>
+
+<body onload="init();">
+<div id="header" class="container-fluid row1">
+	<div class="container">
+    	<div class="row">
+    	 	<div class="col-xs-6" style="padding-top:15px;">
+        		<img src="images/logo.png" alt="Hostrocket"  />
+        	</div>
+        	<div class="col-xs-6" style="padding-top:10px;">
+        		<div class="box f-right t-right" style="width: 146px; height: 43px; opacity:0.5; background-color: #7E7E7E; border-radius: 7px 7px 7px 7px;">
+               <span class="chat-link"><a href="#">Live chat</a></span><a href="#"><img style="height: 43px; width: 47px; vertical-align:middle; margin-left:10px;" src="images/icohelp.png" alt="help" /></a>
+                </div>
+        	</div>
+    	</div>
+		<div class="row">
+    	 	<div class="col-md-7 col-sm-7 col-xs-8" style="padding-top:50px;">  		
+<p class="big-text">
+<span style="color:#423852;">Fast.</span><br>
+<span style="color:#ffffff;">Affordable<br>
+Web Hosting<br>
+Services.</span>
+</p>
+<p class="small-text">
+Web Hosting & Domain Name
+</p><br><br>
+
+        	</div>
+        	<div class="col-md-5 col-sm-5 col-xs-4">
+        		<canvas id="canvas" width="820" height="686"></canvas>
+        	</div>
+    	</div>
+        <div class="row">
+        	<div class="col-md-7 col-xs-12">
+            <div class="form-top">
+Try our cPanel demo
+			</div>
+            </div>
+        </div>
+	</div>
+</div>
+<div id="signup" class="container-fluid row2">
+	<div class="container">
+		<div class="row">
+    	 	<div class="col-md-6 col-sm-6 form-holder">
+        		<form id="demoform" method="post">
+                	<div class="form-items-holder">
+                		<input type="text" name="name" placeholder="Name">
+                    	<input type="text" name="email" placeholder="Email">
+                    	<input type="text" name="domainname" placeholder="Your Domain Name">
+                    	<input type="text" name="username" placeholder="Username">
+                    	<input type="submit" value="Get your free demo">
+                    </div>
+                </form>
+        	</div>
+        	<div class="col-md-6 col-sm-6"  style="padding-top:50px;">
+<p class="big-text">
+<span style="color:#423852;">It's Free.</span><br>
+<span style="color:#999999;">Signup<br>
+today!</span>
+</p>
+<p style="font-size: 12px;color:#999999;">
+This is a paragraph. Click here to add / edit your own text. This should be used to tell a story and let your users know a little more about your product or service. How can you benefit them?
+</p>
+        	</div>
+    	</div>
+	</div>
+</div>
+<div id="trynow" class="container-fluid row3">
+	<div class="container">
+		<div class="row">
+    	 	<div class="col-md-6 col-sm-6 col-xs-12 trynow">
+        		<h3>Try It now</h3><br>
+                <ul>
+                	<li>Unlimited Disk Space & Bandwidth.</li>
+                    <li>Free Website Builder.</li>
+                    <li>Easy to Use Control Panel (cPanel).</li>
+                    <li>Unlimited Email Addresses.</li>
+                </ul>
+                <a href="#" class="btn-learn-more"><div>Learn more</div></a>
+        	</div>
+        	<div class="col-md-6 col-sm-6 col-xs-12 laptop">
+            	<div class="video">
+                	<iframe class="youtube_object" type="text/html" width="337" height="212" src="https://www.youtube.com/embed/7LhtYj4PXkk" frameborder="0" allowfullscreen=""></iframe>
+                </div>
+        		<img src="images/imac.png" alt="">
+        	</div>
+    	</div>
+	</div>
+</div>
+<div id="features" class="container-fluid row4 features">
+	<div class="container">
+    	<div class="row">
+        	<div class="col-md-12 row-title1">
+            	<div>
+               		<span class="text">Features</span><span class="dot">.</span>
+                </div>
+            </div>
+        </div>
+		<div class="row">
+    	 	<div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icoflag.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">Awarded Hosting</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+        	<div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icocostum.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">Customizable</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+           <div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icolaptop.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">Responsive</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+        	<div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icomoney.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">Affordable</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+    	</div>
+        <div class="row">
+    	 	<div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icosales.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">Marketing tools</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+        	<div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icosupport.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">24/7 Support</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+           <div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icocloud.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">Daily Backup</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+        	<div class="col-sm-3 col-xs-6 feature">
+        		<img src="images/icopagebuilder.png" style="height: 127px; width: 125px;" alt=""><br>
+                <div class="f-title">Page builder</div>
+                <div class="f-details">This is a paragraph. Click here to add / edit your own text.</div>
+        	</div>
+    	</div>
+	</div>
+</div>
+<div id="prices" class="container-fluid row5">
+	<div class="container">
+    	<div class="row">
+        	<div class="col-md-12 row-title2">
+            	<div>
+               		<span class="text">Prices</span><span class="dot">.</span>
+                </div>
+            </div>
+        </div>
+		<div class="row">
+    	 	<div class="col-md-3 col-sm-6 price-column">
+        		<div class="price-table">
+                	<div class="price-title green">standard</div>
+                	<div class="price"><span style="font-size:26px;">$</span>38</div>
+                	<div class="duration">/mo</div>
+                	50 GB DISK SPACE
+                	<div class="sp"></div>
+                	5000 GB BANDWIDTH
+                	<div class="sp"></div>
+                	100 EMAIL ACCOUNTS
+                	<div class="sp"></div>
+                	100 FTP ACCOUNTS
+                	<div class="sp"></div>
+                	UNLIMITED FEATURES
+                </div>
+                <a href="#" class="price-btn"><div class="green">Sign up</div></a>
+        	</div>
+        	<div class="col-md-3 col-sm-6 price-column">
+        		<div class="price-table">
+                	<div class="price-title pink">advanced</div>
+                	<div class="price"><span style="font-size:26px;">$</span>57</div>
+                	<div class="duration">/mo</div>
+                	100 GB DISK SPACE
+                	<div class="sp"></div>
+                	5000 GB BANDWIDTH
+                	<div class="sp"></div>
+                	100 EMAIL ACCOUNTS
+                	<div class="sp"></div>
+                	100 FTP ACCOUNTS
+                	<div class="sp"></div>
+                	UNLIMITED FEATURES
+                </div>
+                <a href="#" class="price-btn"><div class="pink">Sign up</div></a>
+                <div class="circle">BEST DEAL!</div>
+        	</div>
+            <div class="col-md-3 col-sm-6 price-column">
+        		<div class="price-table">
+                	<div class="price-title blue">business</div>
+                	<div class="price"><span style="font-size:26px;">$</span>73</div>
+                	<div class="duration">/mo</div>
+                	300 GB DISK SPACE
+                	<div class="sp"></div>
+                	5000 GB BANDWIDTH
+                	<div class="sp"></div>
+                	100 EMAIL ACCOUNTS
+                	<div class="sp"></div>
+                	100 FTP ACCOUNTS
+                	<div class="sp"></div>
+                	UNLIMITED FEATURES
+                </div>
+                <a href="#" class="price-btn"><div class="blue">Sign up</div></a>
+        	</div>
+        	<div class="col-md-3 col-sm-6 price-column">
+        		<div class="price-table">
+                	<div class="price-title purple">altra</div>
+                	<div class="price"><span style="font-size:26px;">$</span>83</div>
+                	<div class="duration">/mo</div>
+                	500 GB DISK SPACE
+                	<div class="sp"></div>
+                	5000 GB BANDWIDTH
+                	<div class="sp"></div>
+                	100 EMAIL ACCOUNTS
+                	<div class="sp"></div>
+                	100 FTP ACCOUNTS
+                	<div class="sp"></div>
+                	UNLIMITED FEATURES
+                </div>
+                <a href="#" class="price-btn"><div class="purple">Sign up</div></a>
+        	</div>
+    	</div>
+	</div>
+</div>
+<div id="testimonials" class="container-fluid row6">
+	<div class="container">
+    	<div class="row">
+        	<div class="col-md-12 row-title2">
+            	<div>
+               		<span class="text">Testimonials</span><span class="dot">.</span>
+                </div>
+            </div>
+        </div>
+		<div class="row">
+    	 	<div class="col-md-12">
+        		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  					<!-- Indicators -->
+  					<ol class="carousel-indicators">
+    					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    					<li data-target="#myCarousel" data-slide-to="1"></li>
+    					<li data-target="#myCarousel" data-slide-to="2"></li>
+    					<li data-target="#myCarousel" data-slide-to="3"></li>
+  					</ol>
+
+  					<!-- Wrapper for slides -->
+  					<div class="carousel-inner" role="listbox">
+    					<div class="item active">
+                        	<div class="testimonial">
+      							<img src="images/alex.png" style="height: 105px; width: 105px;" alt="">
+                            	<div class="testimonial-title">Alex Paul</div>
+                            	<div class="testimonial-subtitle">from Apple.</div>
+                            	<div class="testimonial-details">This is a paragraph. Click here to add / edit your own text. This should be used to tell a story and let your users know a little more about your product or service. How can you benefit them?</div>
+                            </div>
+    					</div>
+
+    					<div class="item">
+      						<div class="testimonial">
+      							<img src="images/alex.png" style="height: 105px; width: 105px;" alt="">
+                            	<div class="testimonial-title">Alex Paul</div>
+                            	<div class="testimonial-subtitle">from Apple.</div>
+                            	<div class="testimonial-details">This is a paragraph. Click here to add / edit your own text. This should be used to tell a story and let your users know a little more about your product or service. How can you benefit them?</div>
+                            </div>
+    					</div>
+
+    					<div class="item">
+      						<div class="testimonial">
+      							<img src="images/alex.png" style="height: 105px; width: 105px;" alt="">
+                            	<div class="testimonial-title">Alex Paul</div>
+                            	<div class="testimonial-subtitle">from Apple.</div>
+                            	<div class="testimonial-details">This is a paragraph. Click here to add / edit your own text. This should be used to tell a story and let your users know a little more about your product or service. How can you benefit them?</div>
+                            </div>
+    					</div>
+
+    					<div class="item">
+      						<div class="testimonial">
+      							<img src="images/alex.png" style="height: 105px; width: 105px;" alt="">
+                            	<div class="testimonial-title">Alex Paul</div>
+                            	<div class="testimonial-subtitle">from Apple.</div>
+                            	<div class="testimonial-details">This is a paragraph. Click here to add / edit your own text. This should be used to tell a story and let your users know a little more about your product or service. How can you benefit them?</div>
+                            </div>
+    					</div>
+  					</div>
+
+  					<!-- Left and right controls -->
+  					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    					<span class="sr-only">Previous</span>
+                        <img  class="glyphicon glyphicon-chevron-left"  src="images/arrowleft.png" style="height: 53px; width: 30px;" alt="">
+  					</a>
+  					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    					<span class="sr-only">Next</span>
+                        <img class="glyphicon glyphicon-chevron-right" src="images/arrowright.png" style="height: 53px; width: 30px;" alt="">
+  					</a>
+				</div>
+        	</div>
+    	</div>
+	</div>
+</div>
+<div id="clients" class="container-fluid row7">
+	<div class="container">
+    	<div class="row">
+        	<div class="col-md-12 row-title2">
+            	<div>
+               		<span class="text">Clients</span><span class="dot">.</span>
+                </div>
+            </div>
+        </div>
+		<div class="row">
+    	 	<div class="col-md-12 clients">
+        		<div class="client-logo"><img src="clients/1.png" alt=""></div>
+                <div class="client-logo"><img src="clients/2.png" alt=""></div>
+                <div class="client-logo"><img src="clients/3.png" alt=""></div>
+                <div class="client-logo"><img src="clients/4.png" alt=""></div>
+                <div class="client-logo"><img src="clients/5.png" alt=""></div>
+        	</div>
+    	</div>
+	</div>
+</div>
+<div id="footer" class="container-fluid row8">
+	<div class="container">
+		<div class="row">
+    	 	<div class="col-md-6 col-sm-6 col-xs-12 footer-left">
+        		All Rights reserved. 2015 © <strong>Website name</strong>.
+        	</div>
+        	<div class="col-md-6 col-sm-6 col-xs-12 footer-right">
+        		<a href="#" class="social"><img src="images/facebookvariation.png" style="height: 32px; width: 32px;" alt=""></a>
+              <a href="#" class="social"><img src="images/linkedinvariation.png" style="height: 32px; width: 32px;" alt=""></a>
+              <a href="#" class="social"><img src="images/twittervariation.png" style="height: 32px; width: 32px;" alt=""></a>
+        	</div>
+    	</div>
+	</div>
+</div>
+<script src="js/jquery-1.11.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script src="js/easeljs-0.6.0.min.js"></script>
+<script src="js/tweenjs-0.4.0.min.js"></script>
+<script src="js/movieclip-0.6.0.min.js"></script>
+<script src="js/preloadjs-0.3.0.min.js"></script>
+<script src="js/rocketanimation.js"></script>
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script>
+"use strict";
+var canvas, stage, exportRoot;
+
+function init() {
+	canvas = document.getElementById("canvas");
+	images = images||{};
+
+	var manifest = [
+		{src:"images/cloud.png", id:"cloud"},
+		{src:"images/rocket.png", id:"rocket_1"}
+	];
+
+	var loader = new createjs.LoadQueue(false);
+	loader.addEventListener("fileload", handleFileLoad);
+	loader.addEventListener("complete", handleComplete);
+	loader.loadManifest(manifest);
+}
+
+function handleFileLoad(evt) {
+	if (evt.item.type == "image") { images[evt.item.id] = evt.result; }
+}
+
+function handleComplete() {
+	exportRoot = new lib.rocket();
+
+	stage = new createjs.Stage(canvas);
+	stage.addChild(exportRoot);
+	stage.update();
+
+	createjs.Ticker.setFPS(30);
+	createjs.Ticker.addEventListener("tick", stage);
+}
+
+</script>
+</body>
+</html>
