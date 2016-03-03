a918f190f775b38b95d412bd9ef4eae9c844b1cb
Vitaliy <1977kvl@gmail.com>
2016-03-03 05:09:17 +0200

    Commit 2016.03.03   05:09

--- my/views/site/partners.php
+++ my/views/site/partners.php
@@ -242,8 +242,8 @@ $this->registerCss($css);
             <p><span style="font-size: 25px;">Мы ценим наших клиентов и партнеров,<br> поэтому отдаем</span> <span style="color: #F64747; font-size: 35px;">
 50%</span> <br>от всех оплат привлеченных Вами пользователей.</p>
             <p>Просто поделись ссылкой и получай пассивный доход.</p>
-            <p> Ваша партнерская ссылка для приглашения в саму систему <a href=\"http://1-mlm.com/ref-{$usrDt->refdt}.html\"
-                 target=\"_blank\">http://1-mlm.com/ref-{$usrDt->refdt}.html</a><br /></p>
+            <p> Ваша партнерская ссылка <?= "<a href=\"http://1-mlm.com/ref-{$usrDt->refdt}.html\"
+                 target=\"_blank\">http://1-mlm.com/ref-{$usrDt->refdt}.html</a>"; ?><br /></p>
                  
                  
 
@@ -252,6 +252,163 @@ $this->registerCss($css);
     </div>
 </section>
             </div>
+            
+                                        <!-- BEGIN PORTLET-->
+                            <div class="portlet light bordered">
+                                <div class="portlet-title">
+                                    <div class="caption">
+                                       
+                                    </div>
+                                    <!--<div class="actions">
+                                        <a class="btn btn-circle btn-icon-only btn-default" href="javascript:;">
+                                            <i class="icon-cloud-upload"></i>
+                                        </a>
+                                        <a class="btn btn-circle btn-icon-only btn-default" href="javascript:;">
+                                            <i class="icon-wrench"></i>
+                                        </a>
+                                        <a class="btn btn-circle btn-icon-only btn-default" href="javascript:;">
+                                            <i class="icon-trash"></i>
+                                        </a>
+                                    </div>-->
+                                </div>
+                                <div class="portlet-body">
+                                    
+                                    <!-- Button to trigger modal -->
+                                    <a href="#myModal1" role="button" class="btn blue" data-toggle="modal"> условия партнерской программы </a>
+                                    
+                                    <!-- Modal -->
+                                    <div id="myModal1" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                                    <h4 class="modal-title">Условия партнерской программы</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                   <div class="wind-body" style="padding: 0 30px;">
+        1. Участником партнерской программы может стать любой зарегистрированный пользователь на тарифе "мастер".
+        <br>
+        2. Используя свою партнерские ссылку, Вы становитесь участником данной
+        партнерской программы и полностью принимаете все ее правила и условия.
+        <br>
+       3. Когда привлеченный Вами клиент выполняет оплату тарифа, Вам начисляется
+        вознаграждение в размере 50% от суммы его первого и всех последующих платежей.
+        <br>
+        3.1. Партнерское вознаграждение начисляется в течение первого года после регистрации
+        пользователя. Дальнейшие вознаграждение с пользователя не начисляются.
+        <br>
+        3.2. Партнерское вознаграждение не начисляется при оплате клиентом дополнительных услуг (в
+        разделе Услуги).
+        <br>
+        4. Участник партнерской программы обязан предоставлять привлекаемым клиентам достоверную
+        информацию об услугах сервиса.
+        <br>
+        5. Запрещено использовать незаконные способы привлечения клиентов или любое другое искусственное
+        увеличение количества заказов.
+        <br>
+        6.1. Запрещенный вид трафика:
+        <br>
+        6.1.1. Email-рассылки (по согласованию)
+        <br>
+        6.1.2. ClickUnder-реклама
+        <br>
+        6.1.3. SMS-рассылки
+        <br>
+        6.1.4. Push-реклама
+        <br>
+        6.1.5. Cookie-Dropping
+        <br>
+        6.2. Запрещенные источники трафика:
+        <br>
+        6.2.1. Социальные сети:
+        <br>
+        6.2.1.1. Создание брендированных групп;
+        <br>
+        6.2.1.2. Реклама через статусы/рекомендации от лица сотрудников 1-го млм Ресурса;
+        <br>
+        6.2.1.3. Мотивированный трафик;
+        <br>
+        6.2.1.4. Офферная реклама.
+        <br>
+        6.3. Запрещено использование собственных рекламных материалов
+        <br>
+        6.2.2. Incentive любой (мотивированный трафик)
+        <br>
+        6.2.3. Контекстная реклама (Yandex Direct, Google Adwords, Begun)
+        Рекламные кампании по словам и словосочетаниям, в которых есть слова: 1 млм Ресурс (любые словосочетания и слова-синонимы);
+        <br>
+        6.2.5. Спам-рассылки (мессенжеры)
+        <br>
+        6.2.6. Adult-трафик
+        <br>
+        6.3. Запрещается использование собственных рекламных материалов
+        <br>
+        7. В случае выявления нарушения любого из пунктов настоящих Правил, начисление вознаграждения не
+        осуществляется. Аккаунт партнера блокируется, без возможности возврата средств с баланса сервиса
+        и партнерского счета.
+        <br>
+        8. Партнерское вознаграждение можно использовать для оплаты услуг сервиса либо вывести в
+        платежную систему Яндекс.Деньги. Для этого нужно сделать заявку в разделе Вывод средств.
+        <br>
+
+        8.1. Минимальная сумма вывода  - 100$
+        <br>
+        9. Сервис "1-й млм Ресурс" не несет ответственности за любые неполадки в работе
+        партнерской программы, простои, ошибки и перебои, а также за любой ущерб, понесенный участником
+        партнерской программы, привлеченным клиентом или третьими лицами в ходе использования данной
+        партнерской программы.
+        <br>
+        10. Сервис "1-й млм Ресурс" оставляет за собой право в любой момент в
+        одностороннем порядке изменить условия данной Партнерской программы, а также условия участия в
+        ней.
+        <br>
+        11. Сервис "1-й млм Ресурс" оставляет за собой право в любой момент в
+        одностороннем порядке прекратить действие данной Партнерской программы.
+    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button class="btn default" data-dismiss="modal" aria-hidden="true">Закрыть</button>
+                                                    <button class="btn yellow">Save</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="myModal2" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                                    <h4 class="modal-title">Alert Header</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p> Body goes here... </p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button data-dismiss="modal" class="btn green">OK</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="myModal3" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel3" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                                    <h4 class="modal-title">Confirm Header</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p> Body goes here... </p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button class="btn default" data-dismiss="modal" aria-hidden="true">Close</button>
+                                                    <button data-dismiss="modal" class="btn blue">Confirm</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END MODAL DIALOG PORTLET-->
             
             <div class="portlet-body">
 <section id="section2" data-scen-id="0" class="builder-section no-padding bg-img ng-touched ng-dirty-parse" ng-mouseup="deTextCustomize()">
